0ef6b862c8682394b694ea2aad45641bb9484d6e
dipanc1 <dipanchhabra@gmail.com>
2022-04-02 08:43:23 +0530

    style sheets, button props and stuff, will clean up after this

--- App.js
+++ App.js
@@ -12,18 +12,48 @@ import {
   Text,
   View,
   Button,
-  Linking,
+  // Linking,
 } from 'react-native';
+import { useState } from 'react/cjs/react.development';
 
 
 const App = () => {
+  // const [name, setName] = useState('Jatt');
+  // const [session, setSession] = useState({ number: 6, title: 'state' });
+  // const [current, setCurrent] = useState(false);
+  const [add, setAdd] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const onClickHandler = () => {
+    // setName('Jatt Di Mashook');
+    // setSession({ number: 700, title: 'state changed' });
+    // setCurrent(!current);
+    setAdd(add + 5);
+    setCount(count + 1);
+  };
 
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>
-        Jatt di Application
+      {/* <Text style={styles.text}>
+        {name} di Application
       </Text>
-      <Button title="jatt" onPress={()=>{Linking.openURL('https://dipan-portfolio.netlify.app');}}>Jatt</Button>
+      <Text style={styles.text}>
+        Session Number: {session.number} and about {session.title}
+      </Text>
+      <Text style={styles.text}>
+        {current ? 'True' : 'False'}
+      </Text> */}
+      <View style={styles.view1}>
+        <Text style={styles.text}>
+          Jatt di Mashook
+        </Text>
+      </View>
+      {/* <Button title="jatt" onPress={()=>{Linking.openURL('https://dipan-portfolio.netlify.app');}}>Jatt</Button> */}
+      {/* <Button title="update state" onPress={onClickHandler}>Jatt</Button> */}
+      {/* <Button color="#f00" title="add" onPress={onClickHandler}>Jatt</Button>
+      <Text style={styles.text}>
+        you clicked {count} times
+      </Text> */}
     </View>
   );
 };
@@ -34,12 +64,33 @@ const styles = StyleSheet.create({
     backgroundColor: '#38ba68',
     alignItems: 'center',
     justifyContent: 'center',
+    // borderWidth: 10,
+    // borderColor: '#f00',
+    // borderRadius: 10,
+    // margin: 10,
   },
-  text:{
+  view1: {
+    width: 200,
+    height: 100,
+    backgroundColor: '#f00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
     color: '#fff',
-    fontSize: 30,
+    fontSize: 20,
     fontStyle: 'italic',
+    margin: 20,
+    // textTransform: 'uppercase',
   },
+  // button: {
+  //   backgroundColor: '#f00',
+  //   fontSize: 20,
+  //   fontStyle: 'italic',
+  //   margin: 20,
+  //   textTransform: 'uppercase',
+  //   width: 100,
+  // },
 });
 
 export default App;
